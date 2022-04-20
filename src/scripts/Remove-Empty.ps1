49866d8a2284f3d85ea433bbc7a60de08adac50a
Decator <nealaidan.dev@gmail.com>
2022-04-20 04:36:45 -0400

    Revert "added basic replacements for some gnu coreutils"
    This reverts commit 66c61943e6c92601dffe4b896a1c88bd38cda433.

---
+++ src/scripts/Remove-Empty.ps1
@@ -0,0 +1,48 @@
+#!/usr/bin/env pwsh
+# Author: Aidan Neal <squidwardnose4507@gmail.com>
+# Maintainer: Aidan Neal 
+# Contact: https://discord.gg/8wBUFeGGYC (Discord)
+# Target platform(s): Windows, Linux, MacOS
+
+param (
+  [Parameter(Mandatory=$true,
+  ValueFromPipeline=$true)]
+  [string]
+  $target_directory
+)
+
+function Exit-code
+{
+  if ( $? )
+  {
+    Throw "`nProgram exited with code: $? [1]`n"
+  }
+  else
+  {
+    Write-Host "`nProgram exited with code: $? [0]`n"
+    
+  }
+}
+
+function Remove-Empty
+{
+  do
+  {
+    $dirs = Get-ChildItem $target_directory -directory -recurse | Where-Object { (Get-ChildItem $_.fullName).count -eq 0 } | Select-Object -expandproperty FullName
+    $dirs | Foreach-Object { Remove-Item $_ }
+  }
+  while ($dirs.count -gt 0) 
+  Exit-Code
+  
+  return "Complete"
+}
+
+
+if ( Test-Path -Path $target_directory )
+{
+  Remove-Empty
+}
+else
+{
+  throw "Cannot find the suppiled path, did you enter it correctly?"
+}
